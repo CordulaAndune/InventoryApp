5643ed9dc5f21ae4be147ac8de38aa9a873fb2e5
Cordula <cordula.gloge@gmx.de>
2018-06-20 17:11:52 +0200

    Initial commit: all files

--- app/src/main/java/de/cordulagloge/android/bookstore/CatalogActivity.java
+++ app/src/main/java/de/cordulagloge/android/bookstore/CatalogActivity.java
@@ -1,14 +1,17 @@
 package de.cordulagloge.android.bookstore;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-import de.cordulagloge.android.bookstore.data.BookContract;
+import javax.xml.transform.Templates;
+
 import de.cordulagloge.android.bookstore.data.BookDbHelper;
 
-import static de.cordulagloge.android.bookstore.data.BookContract.*;
+import static de.cordulagloge.android.bookstore.data.BookContract.BookEntry;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -20,13 +23,13 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
-        bookDbHelper = new BookDbHelper(this, DB_NAME,DB_VERSION);
+        bookDbHelper = new BookDbHelper(this, DB_NAME, DB_VERSION);
     }
 
     /**
      * insert dummy data into database table books
      */
-    private void insertDummyBook(){
+    private void insertDummyBook() {
         SQLiteDatabase bookDB = bookDbHelper.getWritableDatabase();
 
         String bookName = "The Hobbit";
@@ -40,5 +43,34 @@ public class CatalogActivity extends AppCompatActivity {
         values.put(BookEntry.COLUMN_BOOK_SUPPLIER, supplier);
 
         bookDB.insert(BookEntry.TABLE_NAME, null, values);
+    }
+
+    /**
+     * read complete database with all columns and rows
+     */
+    private void readWholeDataBase() {
+        SQLiteDatabase bookDB = bookDbHelper.getReadableDatabase();
+        String[] projection = new String[]{BookEntry._ID
+                , BookEntry.COLUMN_BOOK_NAME
+                , BookEntry.COLUMN_BOOK_PRICE
+                , BookEntry.COLUMN_BOOK_QUANTITY
+                , BookEntry.COLUMN_BOOK_SUPPLIER
+                , BookEntry.COLUMN_BOOK_SUPPLIER_PHONE};
+        Cursor cursor = bookDB.query(BookEntry.TABLE_NAME
+                , projection
+                , null
+                , null
+                , null
+                , null
+                , null);
+        TextView dataTextView = findViewById(R.id.data_text_view);
+        try {
+            while(cursor.moveToNext()){
+
+            }
+        }
+        finally {
+            cursor.close();
+        }
     }
 }
